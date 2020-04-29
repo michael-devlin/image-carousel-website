4fb93224eeca5ee5bb7cd7a701170d32fe29e09e
Michael <michaeldevfay@gmail.com>
2020-04-29 18:47:33 +0100

    Add slide mover function to move images using CSS keyframes

--- javascript.js
+++ javascript.js
@@ -1,11 +1,44 @@
-
 var indexOfSlides,slides;
 function startSlideshow(){
     indexOfSlides = 0;
     slides=document.getElementsByClassName("slide");
     slides[indexOfSlides].style.opacity=1;
 
-
-
 }
 startSlideshow();
+function addSlides(n) {
+    slideMover(indexOfSlides+n);
+}
+
+function slideMover(n){
+    var i;
+    var current,next;
+    var slideMoverAnimation={
+          forCurrent:"",
+          forNext:""
+    };
+
+    if(n>indexOfSlides) {
+        if(n >= slides.length){n=0;}
+        slideMoverAnimation.forCurrent="moveCurrentSlideLeft";
+        slideMoverAnimation.forNext="moveNextSlideLeft";
+    }else if(n<indexOfSlides){
+        if(n<0){n=slides.length-1;}
+        slideMoverAnimation.forCurrent="moveCurrentSlideRight";
+        slideMoverAnimation.forNext="movePrevSlideRight";
+    }
+
+    if(n!=indexOfSlides){
+        next = slides[n];
+        current=slides[indexOfSlides];
+        for (i = 0; i < slides.length; i++) {
+            slides[i].className = "slide";
+            slides[i].style.opacity=0;
+        }
+        current.classList.add(slideMoverAnimation.forCurrent);
+        next.classList.add(slideMoverAnimation.forNext);
+        indexOfSlides=n;
+
+    }
+
+}
